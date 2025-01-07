5c84e28267891c0bf5e092381b64d45957644187
Taras Hamkalo <hamkalo.taras@gmail.com>
2025-01-07 17:10:35 +0100

    feat: posting to mother broker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,33 +5,26 @@ import {MatButton} from "@angular/material/button";
 import {MqttIrService} from "./services/mqtt-ir.service";
 import {IMqttServiceOptions, MqttModule} from "ngx-mqtt";
 
+const MQTT_SERVICE_OPTIONS: IMqttServiceOptions = {
+  hostname: '147.232.205.176',
+  port: 8000,
+  // port: 8088,
+  // path: '/ws',
+  protocol: 'ws',
+
+  clientId: 'th776no',
+  username: 'maker',
+  password: 'mother.mqtt.password',
+};
+
 // const MQTT_SERVICE_OPTIONS: IMqttServiceOptions = {
-//   // hostname: '147.232.205.176',
-//   // hostname: "mosquitto",
-//   hostname: "localhost",
-//   // hostname: 'mosquitto',
+//   hostname: '127.0.0.1',
 //   port: 8000,
-//   path: '/',
-//   protocol: 'ws',
-//
-//   clientId: 'th776no',
-//
-//   // username: 'maker',
-//   // password: 'mother.mqtt.password',
-//
-//   // url: 'ws://147.232.205.176:8000/ws',
-//   // connectOnCreate: false
+//   path: '/', // Default path as Mosquitto is not configured for a custom path
+//   protocol: 'ws', // WebSocket protocol
+//   clientId: 'th776no', // Unique client ID
 // };
 
-const MQTT_SERVICE_OPTIONS: IMqttServiceOptions = {
-  // hostname: "localhost", // Or use "mosquitto" if that's the hostname in your Docker network
-  hostname: '127.0.0.1',
-  // hostname: 'mosquitto',
-  port: 8000,
-  path: '/', // Default path as Mosquitto is not configured for a custom path
-  protocol: 'ws', // WebSocket protocol
-  clientId: 'th776no', // Unique client ID
-};
 @NgModule({
   declarations: [
     AppComponent, // Declare your AppComponent here
