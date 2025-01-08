5a08bb2b4f96625f68f5c4830e5f0dc09a0bc7b2
Taras Hamkalo <hamkalo.taras@gmail.com>
2025-01-08 22:08:29 +0100

    refactor: add tab group to action registration

--- src/app/components/register-action/register-action.component.ts
+++ src/app/components/register-action/register-action.component.ts
@@ -1,6 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {SurfaceComponent} from "../surface/surface.component";
-import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatFormFieldModule, MatLabel} from "@angular/material/form-field";
 import {
   FormBuilder,
   FormGroup,
@@ -14,6 +14,10 @@ import {MatCheckbox} from "@angular/material/checkbox";
 import {MatCardActions, MatCardContent} from "@angular/material/card";
 import {MatButton} from "@angular/material/button";
 import {MqttActionsService} from "../../services/mqtt-actions.service";
+import {MqttIrRecorderService} from "../../services/mqtt-ir-recorder.service";
+import {MatDialog} from "@angular/material/dialog";
+import {RecordIrModal} from "../record-ir-modal/record-ir-modal.component";
+import {MatTab, MatTabGroup} from "@angular/material/tabs";
 
 @Component({
   selector: 'app-register-action',
@@ -27,6 +31,8 @@ import {MqttActionsService} from "../../services/mqtt-actions.service";
     MatCardActions,
     MatButton,
     FormsModule,
+    MatTab,
+    MatTabGroup,
   ],
   templateUrl: './register-action.component.html',
   styleUrl: './register-action.component.css'
@@ -35,8 +41,11 @@ export class RegisterActionComponent {
 
   public readonly actionRegistrationForm: FormGroup;
 
+  private dialog: MatDialog = inject(MatDialog);
+
   constructor(private readonly formBuilder: FormBuilder,
-              private readonly mqttActionsService: MqttActionsService
+              private readonly mqttActionsService: MqttActionsService,
+              private readonly mqttIrRecorderService: MqttIrRecorderService
   ) {
     this.actionRegistrationForm = this.formBuilder.group({
       actionName: ['', [Validators.required, Validators.maxLength(150), Validators.pattern('[a-zA-Z ]*')]],
@@ -68,4 +77,15 @@ export class RegisterActionComponent {
     formDirective.resetForm()
   }
 
+  public recordIrPayload() {
+    console.log("recordIrPayload()");
+    const dialogRef = this.dialog.open(RecordIrModal, {
+      width: "75%",
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(result);
+    });
+    
+  }
 }
