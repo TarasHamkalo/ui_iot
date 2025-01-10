c934bf26554ba032dd4c1c2d554b172dbd7481c7
Taras Hamkalo <hamkalo.taras@gmail.com>
2025-01-10 20:20:01 +0100

    feat: room header

--- src/app/pages/room-control-page/room-control-page.component.ts
+++ src/app/pages/room-control-page/room-control-page.component.ts
@@ -5,10 +5,13 @@ import {
   ContentContainerComponent
 } from "../../components/base/content-container/content-container.component";
 import {RoomControlContextService} from "../../context/room-control-context.service";
-import {MqttWrapperService} from "../../services/mqtt-wrapper.service";
 import {
   ActionRegistrationComponent
 } from "../../components/action-registration/action-registration.component";
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatIconButton} from "@angular/material/button";
+import {Router} from "@angular/router";
+import {PageRoutes} from "../../app.routes";
 
 @Component({
   selector: "app-room-control-page",
@@ -17,17 +20,20 @@ import {
     MatTabsModule,
     ContentContainerComponent,
     ActionRegistrationComponent,
+    MatToolbar,
+    MatIconButton,
   ],
   templateUrl: "./room-control-page.component.html",
   styleUrl: "./room-control-page.component.scss"
 })
 export class RoomControlPageComponent {
 
-  constructor(private roomControlContext: RoomControlContextService,
-              private mqttWrapper: MqttWrapperService) {
+  constructor(protected roomControlContext: RoomControlContextService,
+              private router: Router) {
   }
 
-  send(): void {
-    this.mqttWrapper.publish("bin", "MQTT test");
+  protected navigateHome() {
+    this.roomControlContext.deselectControlledRoom();
+    this.router.navigate([`/${PageRoutes.HOME}`]);
   }
 }
